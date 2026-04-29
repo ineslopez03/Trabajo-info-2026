6ec06512cf85047d519af5baf3cf19371646206e
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-04-29 19:11:35 +0200

    prototipo del MotorJuego.h
    Se han declarado los posibles estados:menu,tablero,arena,fin.
    
    se han declarado los objetos "estado actual" que se expliaca solo y "guardado" que contendrá la informacion de la partida. Tambien se han creado punteros a estructuras de tipo jugador (para dos jjugadores) y para pintar la pantalla segun la condicion. tambien se ha  añadido un flag para gestionar cuando se esta ejecutando el juego y cuando se termina.
    Se incorporan los metodos que permiten inicializar lo que sea que haga falta, el bucle principal y la gestion de los estados.

---
+++ ARCHON/ARCHON/MotorArchon.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "InterfazUsuario.h"
+#include "Jugador.h"
+#include "GestorArchivos.h"
+
+enum class EstadoJuego { MENU, TABLERO, ARENA, FIN };//Se hace enum class para que no se puedan comparar con enteros, y para que no haya confusión con otros estados de otras partes del programa
+
+class MotorJuego
+{
+private:
+	EstadoJuego estadoActual; //Variable para almacenar/conocer el estado actual del juego
+	InterfazUsuario* pantallaActiva; //puntero general para la pantalla activa, que se asignará a cada pantalla concreta según el estado del juego
+	GestorArchivos guardado;//objeto que guardará los datos del juego.
+	Jugador* jugador1;
+	Jugador* jugador2;
+	//punteros que apuntan a los dos jugadores sean reales o IA.
+
+	bool ejecutando; //EN caso de estar jugando vale 1, si terminamos/salimos vale 0.
+public:
+	MotorJuego();//constructor generico 
+	~MotorJuego(); //destructor generico 
+
+	void inicializar();//inicializamos todo lo necesario para el juego.
+	void Bucle();// es el bucle principal del juego, se procesan entradas, se dibuja, etc.
+	void CambiarEstado(EstadoJuego nuevoEstado);//si se da la condición de cambiar de estado llamamos a esta funcion y gestiona el cambio.
