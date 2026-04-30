f85cb9e053a52ca97d0381471132b697f12b1b08
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-04-30 11:58:26 +0200

    Metodo cambiarEstado
    Se hace el metodo cambiar estado, encargado de vaciar el puntero y de asignar a estado actual el estado nuevo. Una vez asignado crea dinamicamente el objeto que vaya a representarse (menu,batalla, arena, fin) y le pasamos la direccion al puntero pantalla activa para usarlo para acceder a los metodos del objeto (sea cual sea).

--- ARCHON/ARCHON/MotorArchon.cpp
+++ ARCHON/ARCHON/MotorArchon.cpp
@@ -18,3 +18,33 @@ MotorArchon::~MotorArchon(){
 	if (jugador1 = !nullptr) delete jugador1;
 	if (jugador2 = !nullptr) delete jugador2;
 }
+
+void MotorArchon::cambiarEstado(EstadoJuego NuevoEstado) {
+	//en el caso de	que vengamos de un estado previo borramos la pantalla y apuntamos a la nada.
+	//si el puntero esta vacio no tengo que hacer nada
+	if (pantallaActiva != nullptr) {
+		delete pantallaActiva;
+		pantallaActiva = nullptr;
+	}
+	estadoActual = NuevoEstado; //asigno directamente el estado actual por el estado que he pasado al metodo.
+
+	//hago que el nuevo estado se represente
+	switch (estadoActual) {
+		case EstadoJuego::MENU//en el caso de que queramos representar el menú
+			pantallaActiva = new MenuPrincipal();//creamos dinamicamente el menu y lo asigno a pantallaActica para su representaicon
+			break;
+		
+		case EstadoJuego::TABLERO//en el caso de que queramos representar el tabelro
+			pantallaActiva = new Tablero();//se le asigna a pantallaActiva la clase Tablero que representara el tablero
+			break;
+		
+		case EstadoJuego::ARENA//en el caso de que queramos representar la arena
+			pantallaActiva = new Arena();//la estrucutura a la que apunta el punero será MenuPrincipal
+			break;
+		
+		case EstadoJuego::FIN//en el caso de que se termine el juego
+			ejecutando = false;//ejecutando a 0 para que salga del bucle principal.
+			break;
+		
+	}
+}
