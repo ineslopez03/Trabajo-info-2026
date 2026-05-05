22393a29debaff4315e92058e4f76be3cf80c462
Pablomunozmoreno <pablo.munoz.moreno@alumnos.upm.es>
2026-05-06 00:27:19 +0200

    codigo main

---
+++ ARCHON/ARCHON/GestorArchivos.cpp
@@ -0,0 +1,1 @@
+#include "GestorArchivos.h"
---
+++ ARCHON/ARCHON/Tablero.h
@@ -0,0 +1,37 @@
+#pragma once
+#include"InterfazUsuario.h"
+#include"Casilla.h"
+#include<SFML/Graphics.hpp>
+
+class Tablero :public InterfazUsuario {
+private:
+	Casilla* matriz[9][9];
+	Casilla* origenSeleccionado;
+	bool primerClicRealizado;
+	sf::Clock relojOscilacion;
+	float anguloRotacion = 0.0f;
+	bool hayCombatePendiente = false;
+	Pieza* atacante = nullptr;
+	Pieza* defensor = nullptr;
+	float tiempoAcumuladoOscilacion = 0.0f;
+	int turnosContados = 0;
+	const float Duracion_fase = 2.0f;
+public:
+	Tablero();
+	~Tablero() override;
+	void procesarEntrada(sf::RenderWindow& ventana);
+	void dibujarPantalla(sf::RenderWindow& ventana);
+	void inicializarTablero();
+	void moverPieza(Casilla* origen, Casilla* destino);
+	bool esMovimientoValido(Casilla* origen, Casilla* destino);
+	bool esAtaqueValido(Casilla* origen, Casilla* destino);
+	bool comprobarVictoria();
+	bool estaEnRango(Casilla* origen, Casilla* destino);
+	void gestionarTurno(Casilla* origen, Casilla* destino);
+	bool getHaycombate() const { return hayCombatePendiente; }
+	void resetCombate() { hayCombatePendiente = false; atacante = nullptr; defensor = nullptr; }
+	Pieza* getAtacante() const { return atacante; }
+	Pieza* getDefensor() const { return defensor; }
+	void rotarTablero();
+
+};
