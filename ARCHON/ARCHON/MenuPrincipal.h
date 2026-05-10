dea7a87398d9b4ef8e331c208e9ce51ec1fa557c
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-05-10 12:55:51 +0200

    Se da forma al Menu Principal
    Se crea el enum con los estados,se declaran vectores de botones, se declara un string que transmitirá el deseo de skin, se declara un metodo privado para inciializar los botnes. Se declaran flags para gestionar los eventos en el motor

--- ARCHON/ARCHON/MenuPrincipal.h
+++ ARCHON/ARCHON/MenuPrincipal.h
@@ -1,14 +1,30 @@
 #pragma once
 #include "InterfazUsuario.h"
 #include <SFML/Graphics.hpp>
-enum class OpcionesMenu{PRINCIPAL,SELECCION_MODO,SELECCION_SKIN};
+#include "Boton.h"
+#include <vector>
+enum class OpcionesMenu{PRINCIPAL,RANKING,SELECCION_MODO,IA_NODISPONIBLE,SELECCION_SKIN,};
+//el menu principal mostrará: iniciar partida,cargar partida//no si muy complejo,ranking,instrucciones,salir.
+//el menu de inciar partida pasará a mostrar: Jugador vs Jugador o Jugador vs IA.
+//si se elige jugador vs IA saldrá un mensaje diciendo que no esta disponible y la opcion de volver.
+//si se elige Jugador vs Jugador se mostrarán las tematicas con las que puede iniciar la partida
+//en principio Archon normal, Harry Potter y Star Wars, luego si eso metemos mas
+//si se elige instrucciones se abrirá un archivo que explique como jugar al juego.
+
 
 class MenuPrincipal:public InterfazUsuario{
+private:
+	std::vector<Boton> botonesMenuPrincipal;//listas de botones para tener todo organizado.
+	std::vector<Boton> botonesSeleccionModo;
+	std::vector<Boton> botonesSeleccionSkin;
+	std::vector<Boton> botonesRanking;
+	std::vector<Boton> botonVolverIA;
+	//meto banderas para leerlas en motorArchon
+	bool iniciarJuego = false;
+	bool contraIA = false;
+	std::string skinSeleccionada;
+	void inicializarBotones();//no hace falta que sea publico.
 
-	struct boton{
-		sf::RectangleShape forma;
-		sf::Text texto;
-	};
 public:
 
 	MenuPrincipal();
