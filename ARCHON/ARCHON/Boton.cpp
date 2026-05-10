2f5b0cab9a9ca88e488a4114f7e225b737fe3b08
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-05-10 11:59:55 +0200

    Se empieza a configurar al boton
    Va a ser boton rectangular, se le pueden asignar dimensiones, se pinta como un rectangulo con borde blanco, el color hay que cambiarlo, valores arbitrarios, se le asigna el texto que puede contener y se centra en el hueco.

--- ARCHON/ARCHON/Boton.cpp
+++ ARCHON/ARCHON/Boton.cpp
@@ -1,1 +1,25 @@
 #include "Boton.h"
+Boton::Boton(float x, float y, float ancho, float alto, std::string textoBoton, sf::Font& fuente)
+{
+	//idmensiones del boton
+	fondo.setPosition({ x, y });//ubicacion del boton
+	fondo.setSize(sf::Vector2f(ancho, alto));//tamaño del boton
+	//coloracion del boton
+	colorNormal = sf::Color(100, 100, 100);//color normal del boton
+	colorEncima = sf::Color(150, 150, 150);//color del boton al pasar por encima
+	fondo.setFillColor(colorNormal);//se pone el color 
+	fondo.setOutlineThickness(2.0f);//grosor del borde, luego lo cambio si eso
+	fondo.setOutlineColor(sf::Color::White);//de color blanco 
+	//configuro el texto del boton
+	texto.setFont(fuente);//selecciono la fuente
+	texto.setString(textoBoton);//seteamos el texto 
+	texto.setCharacterSize(24);//tamaño, se ajusta si hace falta
+	texto.setFillColor(sf::Color::White);//por ahora blanco 
+
+	//lo centro en el boton
+	float PosXTexto = x + (ancho - texto.getLocalBounds().size.x) / 2.0f;
+	//miro el ancho del texto y lo resto al ancho del boton, luego lo divido entre 2 para centrarlo
+	float PosYTexto = y + (alto - texto.getLocalBounds().size.x) / 2.0f;//ajustar el texto en y si hace falta
+	//lo mismo pero con el alto
+	texto.setPosition({ PosXTexto ,PosYTexto });
+}
--- ARCHON/ARCHON/Boton.h
+++ ARCHON/ARCHON/Boton.h
@@ -2,5 +2,17 @@
 #include <SFML/Graphics.hpp>
 class Boton
 {
+private:
+	sf::RectangleShape fondo;
+	sf::Text texto;
+	sf::Color colorNormal;
+	sf::Color colorEncima;
+public:
+	Boton(float x,float y,float ancho, float alto, std::string texto,sf:: Font& fuente );
+	//simplemente le digo la forma del recuadro, el texto a contener y la fuente a usar
+
+	void dibujar(sf::RenderWindow& ventana);
+	void raton(sf::Vector2f posRaton);
+	void setTexto( sf::Text nuevoTexto );
 };
 
