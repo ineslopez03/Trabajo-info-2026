350dcf437b2a8215f72ded1b82be576896990187
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-05-10 21:38:09 +0200

    Menus con titulos

--- ARCHON/ARCHON/MenuPrincipal.cpp
+++ ARCHON/ARCHON/MenuPrincipal.cpp
@@ -166,6 +166,28 @@ void MenuPrincipal::dibujarPantalla(sf::RenderWindow& ventana) {
 
 	ventana.draw(SpriteFondo);//Lo pinto antes porque si no me va a tapar los botones
 
+	std::string titulo = "";
+	switch (EstadoInterno)
+	{
+	case OpcionesMenu::PRINCIPAL: Titulos.setCharacterSize(150); titulo = "ARCHON";
+		break;
+	case OpcionesMenu::RANKING: Titulos.setCharacterSize(100); titulo = "RANKING";
+		break;
+	case OpcionesMenu::SELECCION_MODO: Titulos.setCharacterSize(80); titulo="MODO DE JUEGO";
+		break;
+	case OpcionesMenu::IA_NODISPONIBLE: Titulos.setCharacterSize(50); titulo="DLC-IA POR 99 EUROS";
+		break;
+	case OpcionesMenu::SELECCION_SKIN:Titulos.setCharacterSize(100); titulo="TEMATICA";
+		break;
+	}
+	Titulos.setString(titulo);
+	sf::FloatRect limites = Titulos.getLocalBounds();
+	float posX = (800.0f - limites.size.x) / 2.0f;
+	float posY = 50.0f-limites.position.y;
+	Titulos.setPosition({ posX,posY });
+	ventana.draw(Titulos);
+
+
 	auto dibujarLista = [&](std::vector<Boton>& lista) {
 		for (auto& b : lista) b.dibujar(ventana);
 		};//otra funcion lambda que funciona igual que la de arriba 
@@ -188,6 +210,11 @@ void MenuPrincipal::dibujarPantalla(sf::RenderWindow& ventana) {
 		break;
 	}
 }
+
+
+
+
+
 void MenuPrincipal::inicializarBotones() {
 	struct vboton{
 		float centroMenu_x{250};
