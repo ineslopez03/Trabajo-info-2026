a1160798b1e6ffb7c2d3bb6d134adf75776b60c5
dimiveselinov <dimitar.veselinov@alumnos.upm.es>
2026-05-28 13:44:04 +0200

    Se quita codigo conflictivo para ver bien a dumbledor

--- ARCHON/ARCHON/Hechicero.cpp
+++ ARCHON/ARCHON/Hechicero.cpp
@@ -23,20 +23,7 @@ Hechicero::Hechicero(Bando b, std::string skin)
 }
 
 void Hechicero::dibujarEnArena(sf::RenderWindow& ventana, sf::Vector2f pos, bool mirandoDerecha, std::string skin) {
-    if (skin == "HARRY_POTTER" && bando == Bando::LUZ) {
-        sf::Texture& sheet = Pieza::obtenerTexturaDesdeRuta("imagenes/HP/Arena/Dumbledore_Sheet.png");
-        sprite.setTexture(sheet);
-        sprite.setTextureRect(sf::IntRect({ 5, 5 }, { 40, 60 }));
-
-        sf::FloatRect bounds = sprite.getLocalBounds();
-        sprite.setOrigin({ bounds.size.x / 2.f, bounds.size.y / 2.f });
-        sprite.setPosition(pos);
-
-        // Escala reducida para la arena
-        sprite.setScale({ mirandoDerecha ? 1.3f : -1.3f, 1.3f });
-        ventana.draw(sprite);
-    }
-    else {
+    
         PiezaTeletransporte::dibujarEnArena(ventana, pos, mirandoDerecha, skin);
-    }
+    
 }
